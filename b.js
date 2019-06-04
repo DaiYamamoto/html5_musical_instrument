889ded7cbbf87d3d367ea9c3ac3aeaec622804af
DaiYamamoto01 <dai.yamamoto.ts@mixi.co.jp>
2019-06-04 13:26:45 +0900

    1オクターブ分の鍵盤を配置

--- b.js
+++ b.js
@@ -1,12 +1,38 @@
-d3.select("#result")    // ID名resultの要素を指定
-    .append("svg")  // svg要素を追加
-    .attr("width", 320) // svg要素の横幅を指定
-    .attr("height", 240)    // svg要素の縦幅を指定
-    .append("rect") // 四角形（矩形）を追加。以後のメソッドは、この四角形（矩形）に対しての設定になる
-    .attr("x", 80)  // x座標を指定
-    .attr("y", 60)  // y座標を指定
-    .attr("width", 100) // 横幅を指定
-    .attr("height", 100)    // 縦幅を指定
-    .attr("stroke", "blue") // 青色にする
-    .attr("fill", "cyan")   // 塗りは水色にする
-    .attr("stroke-width", 2)    // 線幅を指定
+
+let svg = d3.select("#result").append("svg")  ;
+
+function createWhiteRectangle(x, y) {
+    svg.append("rect") // 四角形（矩形）を追加。以後のメソッドは、この四角形（矩形）に対しての設定になる
+        .attr("x", x)  // x座標を指定
+        .attr("y", y)  // y座標を指定
+        .attr("width", 80) // 横幅を指定
+        .attr("height", 140)    // 縦幅を指定
+        .attr("stroke", "blue") // 青色にする
+        .attr("fill", "#e7e9df")   // 塗りは水色にする
+        .attr("stroke-width", 2)    // 線幅を指定
+}
+
+function createBlackRectangle(x,y) {
+    svg.append("rect") // 四角形（矩形）を追加。以後のメソッドは、この四角形（矩形）に対しての設定になる
+        .attr("x", x)  // x座標を指定
+        .attr("y", y)  // y座標を指定
+        .attr("width", 80) // 横幅を指定
+        .attr("height", 140)    // 縦幅を指定
+        .attr("stroke", "blue") // 青色にする
+        .attr("fill", "#081745")   // 塗りは水色にする
+        .attr("stroke-width", 2)    // 線幅を指定
+}
+
+function main(){
+    svg.attr("width", 800).attr("height", 400);
+    for(let i=0; i<7; i++){
+        createWhiteRectangle(40 + i*100, 200);
+    }
+    for(let i=0; i<6; i++) {
+        if(i!==2 ) createBlackRectangle(90+ i*100, 40)
+    }
+}
+
+
+
+main();
