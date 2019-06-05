a60a0377b4ba2789427ec98d76bb7d26bad9d876
DaiYamamoto01 <dai.yamamoto.ts@mixi.co.jp>
2019-06-05 13:46:51 +0900

    音を鳴らす

--- b.js
+++ b.js
@@ -1,8 +1,9 @@
 
 let svg = d3.select("#result").append("svg")  ;
 
-function createWhiteRectangle(x, y) {
+function createWhiteRectangle(x, y, id) {
     svg.append("rect") // 四角形（矩形）を追加。以後のメソッドは、この四角形（矩形）に対しての設定になる
+        .attr('id', id)
         .attr("x", x)  // x座標を指定
         .attr("y", y)  // y座標を指定
         .attr("width", 80) // 横幅を指定
@@ -12,8 +13,9 @@ function createWhiteRectangle(x, y) {
         .attr("stroke-width", 2)    // 線幅を指定
 }
 
-function createBlackRectangle(x,y) {
+function createBlackRectangle(x,y,id) {
     svg.append("rect") // 四角形（矩形）を追加。以後のメソッドは、この四角形（矩形）に対しての設定になる
+        .attr('id', id)
         .attr("x", x)  // x座標を指定
         .attr("y", y)  // y座標を指定
         .attr("width", 80) // 横幅を指定
@@ -26,13 +28,34 @@ function createBlackRectangle(x,y) {
 function main(){
     svg.attr("width", 800).attr("height", 400);
     for(let i=0; i<7; i++){
-        createWhiteRectangle(40 + i*100, 200);
+        createWhiteRectangle(40 + i*100, 200,idWrap(i));
     }
-    for(let i=0; i<6; i++) {
-        if(i!==2 ) createBlackRectangle(90+ i*100, 40)
+    for(let i=0; i<5; i++) {
+        let x = i < 2 ? 90+ i*100 : 90 + i * 100 + 100;
+        createBlackRectangle(x, 40,idWrap(i+100))
     }
+
+    $("rect").click(function () {
+      //  synth.triggerAttackRelease('C5', '4n');
+        console.log(23)
+        const id = $(this).attr('id');
+
+        playSound(id)
+    });
 }
 
+function idWrap(i) {
+    return i+1;
+}
 
+const synth = new Tone.Synth().toMaster();
+
+const MAP = { 1: 'C5', 2: 'D5', 3:'E5',4:'F5',5:'G5',6:'A5',7:'B5',
+    101: 'C#5',102: 'D#5',103: 'F#5',104: 'G#5',105: 'A#5',
+};
+function playSound(id) {
+    console.log(MAP[id]);
+    synth.triggerAttackRelease(MAP[id], '4n');
+}
 
 main();
