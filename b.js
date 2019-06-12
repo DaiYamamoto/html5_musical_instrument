bc2d7a7afd75903ee50e3d26e33a28ff393bb129
DaiYamamoto01 <dai.yamamoto.ts@mixi.co.jp>
2019-06-12 13:15:07 +0900

    Otoクラスを作成

--- b.js
+++ b.js
@@ -24,52 +24,88 @@ function createBlackRectangle(x,y,id) {
         .attr("fill", "#081745")   // 塗りは水色にする
         .attr("stroke-width", 2)    // 線幅を指定
 }
+const WHITE_KEY = [0,2,4,5,7,9,11]
+const BLACK_KEY  = [1,3,999,6,8,10]; //999 is dummy.
 
+function createKey(num) {
+    if(  WHITE_KEY.includes(num) ) {
+        let index = WHITE_KEY.indexOf(num);
+        createWhiteRectangle(40 + index*100, 200, num);
+    }
+    if (BLACK_KEY.includes(num) ){
+        let index = BLACK_KEY.indexOf(num);
+        createBlackRectangle( 90+ index*100 , 40, num)
+    }
+}
 function main(){
-    svg.attr("width", 800).attr("height", 400);
-    for(let i=0; i<7; i++){
-        createWhiteRectangle(40 + i*100, 200,idWrap(i));
-    }
-    for(let i=0; i<5; i++) {
-        let x = i < 2 ? 90+ i*100 : 90 + i * 100 + 100;
-        createBlackRectangle(x, 40,idWrap(i+100))
-    }
+    const octave = 2;
+    svg.attr("width", 800*octave).attr("height", 400);
+    for(let i=0;i<12;i++ ) createKey(i);
 
+    let oto = new Oto('c_major');
     $("rect").click(function () {
-      //  synth.triggerAttackRelease('C5', '4n');
-        console.log(23)
+
         const id = $(this).attr('id');
 
-        playSound(id)
+        oto.playSound(id);
     });
+    function initSelect() {
+        $('#select').change(function () {
+            const val = $(this).val();
+            console.log(val);
+            oto = new Oto(val);
+        })
+    }
     initSelect();
 }
 
-function idWrap(i) {
-    return i+1;
-}
+const SYNTH = new Tone.Synth().toMaster();
+const FIRST_NOTE = { 'c_major': 'C1','d_major': 'D1'};
 
-const synth = new Tone.Synth().toMaster();
+class Oto{
+    constructor(scaleName){
+        this.list = this.init(scaleName);
+        this.oct  = 4;
+    }
+    init(scaleName){
+        const scale = [];
+        scale[0] =  FIRST_NOTE[scaleName];
+        for(let i =0;i<12*7;i++) {
+            scale[i + 1] = this.raiseHarf(scale[i]);
+        }
+        return scale;
+    }
+    raiseHarf(moji){
+        let key = moji.substr(0,1)
+        let oct = moji.slice(-1)
+        let len = moji.length;
 
-const C_MAJOR = { 1: 'C5', 2: 'D5', 3:'E5',4:'F5',5:'G5',6:'A5',7:'B5',
-    101: 'C#5',102: 'D#5',103: 'F#5',104: 'G#5',105: 'A#5',
-};
-const D_MAJOR = {1: 'D5', 2: 'E5', 3:'F#5',4:'G5',5:'A5',6:'B5',7:'C#6',
-    101: 'D#5',102: 'E#5',103: 'G#5',104: 'A#5',105: 'B#5',
-};
-let mapData = C_MAJOR;
-function playSound(id) {
-    console.log(mapData[id]);
-    synth.triggerAttackRelease(mapData[id], '4n');
+        if( len === 2){
+            let f = key === 'C' || key === 'D' || key === 'F' || key === 'G' || key === 'A' ;
+
+            if (f) return key + "#" + oct;
+            if( key === 'E') return 'F' + oct;
+
+            if ('B' ===key ) return 'C' +  ( parseInt(oct,10) + 1) ;
+        }
+        if (len === 3){
+            let f = key === 'C' || key === 'D' || key === 'F' || key === 'A';
+            if(f) {
+                let codeNum = key.charCodeAt(0)
+                return  String.fromCharCode(codeNum + 1) + oct;
+            }
+
+            if( 'G' === key)   return 'A' + oct ;
+        }
+    }
+    setOct(n){
+        this.oct = n - 1;
+    }
+    playSound(id){
+        let absId =  parseInt(id, 10)  + this.oct * 12;
+        let note = this.list[absId];
+        SYNTH.triggerAttackRelease(note, '4n');
+    }
 }
 
 main();
-
-function initSelect() {
-    $('#select').change(function () {
-        const val = $(this).val();
-        console.log(val);
-        if(val === 'c_major') mapData = C_MAJOR;
-        if(val === 'd_major') mapData = D_MAJOR;
-    })
-}
